ce855417cf9ef9dcf2cd9462b5f8df60a3775eb0
Mia <daffadev.inc@gmail.com>
2025-04-12 11:08:43 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,25 @@ document.addEventListener("click", function() {
         }
     }
 });
+
+    let backPressCount = 0;
+
+    async function showToast(message) {
+      const toast = document.createElement('ion-toast');
+      toast.message = message;
+      toast.duration = 2000; // Durasi toast dalam milidetik
+      document.body.appendChild(toast);
+      await toast.present();
+    }
+
+    window.addEventListener('popstate', function(event) {
+      if (backPressCount === 0) {
+        backPressCount++;
+        showToast('Press back again to exit.');
+        history.pushState(null, null, location.href);
+      } else {
+        window.close();
+      }
+    });
+
+    window.history.pushState(null, null, location.href);
