535f315a7c8d057ec7322b402c6b4d143eee0cd7
Mia <daffadev.inc@gmail.com>
2025-04-12 14:43:28 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("click", function() {
+/*document.addEventListener("click", function() {
     let elem = document.documentElement;
     
     if (!document.fullscreenElement && !document.webkitFullscreenElement) {
@@ -8,7 +8,7 @@ document.addEventListener("click", function() {
             elem.webkitRequestFullscreen();
         }
     }
-});
+});*/
 
     let backPressCount = 0;
 
