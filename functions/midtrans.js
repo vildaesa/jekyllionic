f00e18c5e77ed6ea80717d68170314dba77b85c1
Vilda Esa <bvb7h6dhfx@privaterelay.appleid.com>
2026-05-20 23:58:17 +0700

    Update midtrans.js

--- functions/midtrans.js
+++ functions/midtrans.js
@@ -17,7 +17,7 @@ export async function onRequestPost(context) {
     const response = await fetch("https://app.sandbox.midtrans.com/snap/v1/transactions", {
       method: "POST",
       headers: {
-        "Authorization": "Basic " + btoa("{{site.env.SERVER_KEY}}" + ":"),
+        "Authorization": "Basic " + btoa("{{site.SERVER_KEY}}" + ":"),
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
