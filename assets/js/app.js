02c7732f1a6ad188b7718a031dcaa35917aa5552
Vilda Esa <bvb7h6dhfx@privaterelay.appleid.com>
2026-05-26 12:06:25 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,24 +10,38 @@
     }
 });*/
 
+{% unless pages.url == '/sukses' %}
     let backPressCount = 0;
+    let backPressTimeout;
 
     async function showToast(message) {
       const toast = document.createElement('ion-toast');
       toast.message = message;
-      toast.duration = 2000; // Durasi toast dalam milidetik
+      toast.duration = 2000;
       document.body.appendChild(toast);
       await toast.present();
     }
 
+    window.history.pushState(null, null, location.href);
+
     window.addEventListener('popstate', function(event) {
       if (backPressCount === 0) {
         backPressCount++;
-        showToast('Press back again to exit.');
-        history.pushState(null, null, location.href);
+        showToast('Tekan sekali lagi untuk keluar.');
+        
+        window.history.pushState(null, null, location.href);
+
+        backPressTimeout = setTimeout(() => {
+          backPressCount = 0;
+        }, 2000);
+
       } else {
-        window.close();
+        clearTimeout(backPressTimeout);
+        if (window.navigator.app) {
+            window.navigator.app.exitApp();
+        } else {
+            window.location.href = '/'; 
+        }
       }
     });
-
-    window.history.pushState(null, null, location.href);
+{% endunless %}
